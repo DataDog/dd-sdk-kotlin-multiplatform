13df5a04f983208b6a4cbeadcd653d91ce0d61b6
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2024-04-24 15:42:36 +0200

    RUM-4271: Add iOS and Android SDKs as dependencies for the core module

--- dd-sdk-kotlin-multiplatform-core/build.gradle.kts
+++ dd-sdk-kotlin-multiplatform-core/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.datadog.build.AndroidConfig
+import org.jetbrains.kotlin.gradle.targets.native.tasks.PodGenTask
+
 /*
  * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
  * This product includes software developed at Datadog (https://www.datadoghq.com/).
@@ -6,11 +9,46 @@
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinCocoapods)
     alias(libs.plugins.androidLibrary)
     id("datadog-build-config")
 }
 
 kotlin {
+    cocoapods {
+        // cannot use noPodSpec, because of https://youtrack.jetbrains.com/issue/KT-63331
+        // so what is below for podspec description is just a fake thing to make tooling happy
+        version = AndroidConfig.VERSION.toString()
+        // need to build with XCode 15
+        ios.deploymentTarget = "12.0"
+        name = "DatadogKMPCore"
+        summary = "Official Datadog KMP SDK for iOS."
+
+        framework {
+            baseName = "DatadogKMPCore"
+        }
+
+        pod("DatadogObjc") {
+            extraOpts += listOf(
+                // proposed by KMP because of the @import usage in the binary
+                "-compiler-option",
+                "-fmodules",
+                // see https://youtrack.jetbrains.com/issue/KT-61799
+                // TL;DR: Kotlin interop adds "<ClassName>Meta" class for every "<ClassName>" class,
+                // so since there is DDRUMErrorEventError, it generates DDRUMErrorEventErrorMeta, but such
+                // class is already declared, leading to error: 'DDRUMErrorEventErrorMeta' is going
+                // to be declared twice
+                "-compiler-option",
+                "-DDDRUMErrorEventErrorMeta=DDRUMErrorEventErrorMetaUnavailable"
+            )
+            version = libs.versions.datadog.ios.get()
+        }
+
+        // XCode 15 requires IPHONEOS_DEPLOYMENT_TARGET = 12 at least
+        // filed https://youtrack.jetbrains.com/issue/KT-67757
+        applyXCode15BuildWorkaroundForDatadogPods()
+    }
+
     sourceSets {
         commonMain.dependencies {
             // put your multiplatform dependencies here
@@ -21,6 +59,33 @@ kotlin {
     }
 }
 
+fun applyXCode15BuildWorkaroundForDatadogPods() {
+    tasks.withType<PodGenTask>().configureEach {
+        doLast {
+            val file = project.layout.buildDirectory
+                .file("cocoapods/synthetic/ios/Podfile")
+                .get()
+                .asFile
+            val lines = file.readLines()
+            val output = file.bufferedWriter()
+
+            output.use {
+                for (line in lines) {
+                    output.write(
+                        line.replace(" 11 ", " 12 ")
+                            .replace("{11}", "{12}")
+                    )
+                    output.write(("\n"))
+                }
+            }
+        }
+    }
+}
+
 android {
-    namespace = "com.datadog.kmp.core"
+    namespace = "com.datadog.kmp"
+
+    dependencies {
+        implementation(libs.datadog.android.core)
+    }
 }
