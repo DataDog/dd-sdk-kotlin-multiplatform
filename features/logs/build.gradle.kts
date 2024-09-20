144b5ef5880ca691b7862ef85bdb502d5b3dde2a
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2024-09-20 14:53:33 +0200

    Lift CrashReporter linker workaround to the convention plugin

--- features/logs/build.gradle.kts
+++ features/logs/build.gradle.kts
@@ -6,7 +6,6 @@
 
 import com.datadog.build.plugin.jsonschema.SchemaLocation
 import dev.mokkery.MockMode
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithSimulatorTests
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -62,28 +61,6 @@ kotlin {
             implementation(libs.kotlin.test)
             implementation(projects.tools.unit)
         }
-    }
-
-    targets.withType<KotlinNativeTargetWithSimulatorTests> {
-        compilations
-            .getByName("test")
-            .compileTaskProvider {
-                compilerOptions {
-                    freeCompilerArgs.addAll(
-                        listOf(
-                            "-linker-options",
-                            // TODO RUM-6046 Name of CrashReporter framework is not passed, so have
-                            //  to pass it explicitly, otherwise konanc invocation with linking (ld) fails
-                            //  to locate framework for PLCrashReporter pod. Kotlin Compiler bug?
-                            "-framework CrashReporter " +
-                                // TODO RUM-6047 Kotlin Compiler cannot locate these during the linking
-                                //  done via pods integration
-                                "-U __swift_FORCE_LOAD_\$_swiftCompatibility56 " +
-                                "-U __swift_FORCE_LOAD_\$_swiftCompatibilityConcurrency"
-                        )
-                    )
-                }
-            }
     }
 }
 
