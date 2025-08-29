46f863b826f11e4d7d7e0e6bbf37a1ef7a2b07e7
Nikita Ogorodnikov <nikita.ogorodnikov@datadoghq.com>
2025-08-29 18:51:50 +0200

    Fix flaky DefaultSwiftUIRUMActionsPredicateTest

--- features/rum/src/appleTest/kotlin/com/datadog/kmp/rum/tracking/DefaultSwiftUIRUMActionsPredicateTest.kt
+++ features/rum/src/appleTest/kotlin/com/datadog/kmp/rum/tracking/DefaultSwiftUIRUMActionsPredicateTest.kt
@@ -7,24 +7,46 @@
 package com.datadog.kmp.rum.tracking
 
 import com.datadog.tools.random.randomBoolean
+import kotlinx.cinterop.useContents
+import platform.Foundation.NSProcessInfo
+import platform.UIKit.UIDevice
+import platform.UIKit.UIUserInterfaceIdiomPad
+import platform.UIKit.UIUserInterfaceIdiomPhone
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class DefaultSwiftUIRUMActionsPredicateTest {
 
     @Test
-    fun `M call native default SwiftUI RUM actions predicate W createView`() {
+    fun `M call native default SwiftUI RUM actions predicate W createAction`() {
         // Given
-        val testedInstance = DefaultSwiftUIRUMActionsPredicate(randomBoolean())
+        val fakeLegacyDetectionEnabled = randomBoolean()
+        val testedInstance = DefaultSwiftUIRUMActionsPredicate(fakeLegacyDetectionEnabled)
         val fakeSwiftUIComponentName = "fake-swiftui-component"
 
         // When
         val rumAction = testedInstance.createAction(fakeSwiftUIComponentName)
 
         // Then
-        checkNotNull(rumAction)
-        assertEquals(fakeSwiftUIComponentName, rumAction.name)
-        assertTrue(rumAction.attributes.isEmpty())
+        val majorVersion = NSProcessInfo.processInfo.operatingSystemVersion.useContents { majorVersion }
+        val isIOS = UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPhone ||
+            UIDevice.currentDevice.userInterfaceIdiom == UIUserInterfaceIdiomPad
+        val isIOS18OrLater = isIOS && majorVersion >= 18
+
+        if (isIOS) {
+            if (isIOS18OrLater || fakeLegacyDetectionEnabled) {
+                checkNotNull(rumAction)
+                assertEquals(fakeSwiftUIComponentName, rumAction.name)
+                assertTrue(rumAction.attributes.isEmpty())
+            } else {
+                assertNull(rumAction)
+            }
+        } else {
+            checkNotNull(rumAction)
+            assertEquals(fakeSwiftUIComponentName, rumAction.name)
+            assertTrue(rumAction.attributes.isEmpty())
+        }
     }
 }
