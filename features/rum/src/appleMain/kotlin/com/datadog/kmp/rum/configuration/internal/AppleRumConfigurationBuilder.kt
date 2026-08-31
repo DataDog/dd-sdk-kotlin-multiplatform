f15fc87c9cab30074302b907f3774819d29eb664
Timur Valeev <timur.valeev@datadoghq.com>
2026-08-31 17:48:49 +0100

    Revert "Supporting the timeseries"
    This reverts commit cfc5b5ce23f975422c51f7ab05413aeb14245508.

--- features/rum/src/appleMain/kotlin/com/datadog/kmp/rum/configuration/internal/AppleRumConfigurationBuilder.kt
+++ features/rum/src/appleMain/kotlin/com/datadog/kmp/rum/configuration/internal/AppleRumConfigurationBuilder.kt
@@ -9,8 +9,6 @@ package com.datadog.kmp.rum.configuration.internal
 import cocoapods.DatadogRUM.DDRUMAction
 import cocoapods.DatadogRUM.DDRUMConfiguration
 import cocoapods.DatadogRUM.DDRUMErrorEventErrorCauses
-import cocoapods.DatadogRUM.DDRUMTimeseriesConfiguration
-import cocoapods.DatadogRUM.DDRUMTimeseriesType
 import cocoapods.DatadogRUM.DDRUMView
 import cocoapods.DatadogRUM.DDRUMVitalsFrequency
 import cocoapods.DatadogRUM.DDRUMVitalsFrequencyAverage
@@ -24,7 +22,6 @@ import com.datadog.kmp.event.EventMapper
 import com.datadog.kmp.internal.eraseKeyType
 import com.datadog.kmp.rum.configuration.RumSessionListener
 import com.datadog.kmp.rum.configuration.TimeseriesConfiguration
-import com.datadog.kmp.rum.configuration.TimeseriesType
 import com.datadog.kmp.rum.configuration.VitalsUpdateFrequency
 import com.datadog.kmp.rum.event.ViewEventMapper
 import com.datadog.kmp.rum.model.ActionEvent
@@ -264,9 +261,7 @@ internal abstract class AppleRumConfigurationBuilder : PlatformRumConfigurationB
     }
 
     override fun setTimeseriesConfiguration(configuration: TimeseriesConfiguration) {
-        nativeConfiguration.setTimeseriesConfiguration(
-            DDRUMTimeseriesConfiguration(configuration.enabledTypes.map { it.native })
-        )
+
     }
 
     fun setUiKitViewsPredicate(uiKitViewsPredicate: UIKitRUMViewsPredicate) {
@@ -365,12 +360,6 @@ internal abstract class AppleRumConfigurationBuilder : PlatformRumConfigurationB
         const val MILLISECONDS_IN_SECOND = 1000
     }
 }
-
-private val TimeseriesType.native: DDRUMTimeseriesType
-    get() = when (this) {
-        TimeseriesType.CPU -> DDRUMTimeseriesType.cpu()
-        TimeseriesType.MEMORY -> DDRUMTimeseriesType.memory()
-    }
 
 private val VitalsUpdateFrequency.native: DDRUMVitalsFrequency
     get() = when (this) {
--- features/rum/src/commonMain/kotlin/com/datadog/kmp/rum/configuration/RumConfiguration.kt
+++ features/rum/src/commonMain/kotlin/com/datadog/kmp/rum/configuration/RumConfiguration.kt
@@ -216,7 +216,7 @@ class RumConfiguration internal constructor(internal val nativeConfiguration: An
         }
 
         /**
-         * Enables device timeseries collection.
+         * Enables device timeseries collection (Android only, no-op on other platforms).
          *
          * By default, all supported timeseries types are collected. Use
          * [TimeseriesConfiguration.enabledTypes] to restrict collection to specific types.
